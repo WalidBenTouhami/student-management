f5186758080c33a6d72b94116f56e2e043c0ec11
WalidBenTouhami <walid.bentouhami@esprit.tn>
2026-06-13 13:04:20 +0100

    feat: implement Student and Enrollment service layers and Student repository

--- src/main/java/tn/esprit/studentmanagement/repositories/StudentRepository.java
+++ src/main/java/tn/esprit/studentmanagement/repositories/StudentRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import tn.esprit.studentmanagement.entities.Student;
 
+import java.util.List;
+
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
     boolean existsByEmail(String email);
+    List<Student> findByDepartment_Id(Long departmentId);
 }
