540a12a011c2d32fe65f62aa8bbdbad2aa2b93d9
WalidBenTouhami <walid.bentouhami@esprit.tn>
2026-06-25 17:06:55 +0100

    Test complet CI/CD automatique

--- src/main/java/tn/esprit/studentmanagement/StudentManagementApplication.java
+++ src/main/java/tn/esprit/studentmanagement/StudentManagementApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StudentManagementApplication {
 
     public static void main(String[] args) {
+        // Test complet CI/CD automatique
         SpringApplication.run(StudentManagementApplication.class, args);
     }
 
