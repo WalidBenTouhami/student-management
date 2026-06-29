6310b8d89ee9ee36cc200bc715ae9390e2f87844
WalidBenTouhami <walid.bentouhami@esprit.tn>
2026-06-29 10:59:36 +0100

    feat: add Kubernetes installation script and implement Department REST controller

--- scripts/install-k8s.sh
+++ scripts/install-k8s.sh
@@ -23,8 +23,7 @@ sudo -u vagrant minikube addons enable ingress
 
 echo "📦 Installation de Helm..."
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-chmod 700 get_helm.sh
-./get_helm.sh
+bash ./get_helm.sh
 rm get_helm.sh
 
 echo "⚙️  Configuration des permissions pour Jenkins..."
